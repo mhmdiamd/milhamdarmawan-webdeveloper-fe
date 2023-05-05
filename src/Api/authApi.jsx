752e033945825eeab6c82cee457b18e7b62e3f38
mhmdiamd <darmawanilham34@gmail.com>
2023-05-05 22:54:39 +0700

    edit cors

--- src/Api/authApi.jsx
+++ src/Api/authApi.jsx
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: import.meta.env.VITE_API_ENDPOINT,
-  credentials: 'include',
-});
+  credentials: 'same-origin',
+}); 
 
 export const apiSlice = createApi({
   baseQuery: baseQuery,
